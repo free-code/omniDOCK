7e44f2492140d3c69f8a6f63fddac2f6a99962e3
Josh Price <Price.JoshuaD@Gmail.com>
2010-08-30 16:18:21 -0700

    added comments

--- sandbox/window_and_table.py
+++ sandbox/window_and_table.py
@@ -7,31 +7,33 @@ class MainWindow(gtk.Window):
     #Running gtk.Window's init method
     super(MainWindow, self).__init__() 
     
-    self.set_size_request(400,400)
+    self.set_size_request(280,700)
     #connect gui close button to quit signal
     self.connect("destroy", gtk.main_quit)
     
-    self.table = gtk.Table(8,8,True)
+    #The table is the real gui, the window just holds it.  
+    #Gizmos are added to the table, not the window.  
+    self.table = gtk.Table(12,6,True)
     
-    
-    #--------------------------------------------------
-    # This is all junk code for teasting the XML
-    #
     launchers = self.get_launchers()
     for i, launcher in enumerate(launchers):
       self.table.attach(launcher, i,i+1,1,2)
-    
+      
+    #add the table to the window
     self.add(self.table)
+    #if you don't show or show_all, no gui
     self.show_all()
 
 
   def get_launchers(self):
+    """The "builder" object is the bridge between XML and GTK
+    we create a builder from XML, use it to get a list of objects (buttons)
+    and pass the buttons back in a list"""
     builder = gtk.Builder()
     if builder.add_from_file("button.xml"):
       print "build apparently successful"
     else:
       print "build apparently failed"
-      
     launchers = [x for x in builder.get_objects()]
     return launchers
 
