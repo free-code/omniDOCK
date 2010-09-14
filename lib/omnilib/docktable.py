dc9f2747cef8c10d22f5fb576d49cd26386bfe36
Josh Price <Price.JoshuaD@Gmail.com>
2010-09-13 23:31:36 -0700

    launchers launch

--- lib/omnilib/docktable.py
+++ lib/omnilib/docktable.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
 import gtk
 from xml.etree.ElementTree import ElementTree
+from subprocess import Popen
 
 class DockTable(gtk.Table):
     def __init__(self):
-	
 	self.configTree = self.get_config()
 	rows = int(self.configTree.findtext("rows"))
 	columns = int(self.configTree.findtext("columns"))
-        #self.set_homogeneous(True)
         super(DockTable, self).__init__(rows, columns, homogeneous=True)
         self.add_launchers()
-	#return self
+	
 	
     def add_launchers(self):
 	for launcher in self.configTree.findall("launcher"):
-	    print "found a launcher"
 	    button = gtk.Button()
 	    image  = gtk.Image()
 	    image.set_from_file(launcher.findtext("icon"))
 	    button.set_image(image)
+	    command = launcher.findtext("exec")
+	    print "command = ", command
+	    button.connect("clicked", self.launch, command)
 	    self.attach(button,
 	                int(launcher.findtext("left_attach")),
 	                int(launcher.findtext("right_attach")),
 	                int(launcher.findtext("top_attach")),
 	                int(launcher.findtext("bottom_attach")))
+	                
 	
     def get_free_cells(self):
+	#This is the function from the awesome dude on StackOverflow.  
+	#Thanks Geoff!
 	table = self
         free_cells = set([(x,y) for x in range(table.props.n_columns) for y in range(table.props.n_rows)])
 	def func(child):
@@ -35,9 +39,12 @@ class DockTable(gtk.Table):
 	    free_cells.difference_update(used_cells)
 	table.foreach(func)
         return free_cells
+        
+        
+    def launch(self, button, command):
+	self.running_process = Popen([command])
     
     def get_config(self):
-	print "loading config"
     	etree = ElementTree()
     	config = etree.parse("config/docktable.xml")
     	return config
