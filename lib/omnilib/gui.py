834c2d36c162c6d04ce5b11d2d6fe9ebc0872f4f
Josh Price <Price.JoshuaD@Gmail.com>
2010-09-14 00:24:22 -0700

    window placement and transparency now based on XML settings

--- lib/omnilib/gui.py
+++ lib/omnilib/gui.py
@@ -12,9 +12,14 @@ class OmniDOCKGUI(gtk.Window):
 	self.connect('destroy', gtk.main_quit)
 	self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("black"))
 	self.set_decorated(False)
-	self.set_opacity(.5)
+	opacity = float(configTree.findtext("window/opacity"))
+	self.set_opacity(opacity)
+	self.set_keep_above(True)
 	#self.set_position(gtk.WIN_POS_RIGHT)
-	#self.set_gravity(gtk.gdk.GRAVITY_NORTH_EAST)
+	#self.set_gravity(gtk.gdk.GRAVITY_NORTH_WEST)
+	xpos = int(configTree.findtext("window/xpos"))
+	ypos = int(configTree.findtext("window/ypos"))
+	self.move(xpos,ypos)
 	#Can't seem to figure out how to align to right side
 	
     def add_docktable(self):
