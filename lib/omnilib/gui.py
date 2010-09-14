d6a7c4ac7ce36df5b5ed5c380e008c749537a65e
Josh Price <Price.JoshuaD@Gmail.com>
2010-09-13 20:06:49 -0700

    GUI now displays, imports table which pulls config from XML and populates launchers based on XML input, including launcher position.  Yay =)

--- lib/omnilib/gui.py
+++ lib/omnilib/gui.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import gtk
+import docktable
 
 class OmniDOCKGUI(gtk.Window):
     def __init__(self, configTree):
@@ -7,10 +8,19 @@ class OmniDOCKGUI(gtk.Window):
         height = int(configTree.findtext("window/height"))
         width = int(configTree.findtext("window/width"))
 	self.set_size_request(width, height)
+	self.add_docktable()
+	self.connect('destroy', gtk.main_quit)
+	
+    def add_docktable(self):
+	print "adding docktable"
+	table = docktable.DockTable()
+	self.add(table)
 	
     def show_window(self):
-	self.show()
+	self.show_all()
 	gtk.main()
+	
+    
 
 
 	
