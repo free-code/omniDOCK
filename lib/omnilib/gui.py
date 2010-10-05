0a4e68ed3c28aae63f6f2dd4788f4a52937c5967
Josh Price <Price.JoshuaD@Gmail.com>
2010-10-05 11:51:43 -0700

    FINALLY working non-overlap.  Windows can be maximized without clobbering the dock.

--- lib/omnilib/gui.py
+++ lib/omnilib/gui.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import gtk
 import docktable
+from Xlib import X, display, Xutil, Xatom
 
 class OmniDOCKGUI(gtk.Window):
     def __init__(self):
-	super(OmniDOCKGUI, self).__init__()
+	super(OmniDOCKGUI, self).__init__(gtk.WINDOW_TOPLEVEL)
 	self.configTree = None
 	self.connect('destroy', gtk.main_quit)
 	self.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)
+	#self.set_keep_above(True)
 	
     def add_docktable(self):
 	table = docktable.DockTable()
@@ -15,16 +17,24 @@ class OmniDOCKGUI(gtk.Window):
 
 
     def show_window(self):
-	self.show_all()
 	gtk.main()
+
+    def apply_strut(self, align):
+	top = self.get_toplevel().window
+	if align == "left":
+	    top.property_change("_NET_WM_STRUT", "CARDINAL", 32, 
+	                        gtk.gdk.PROP_MODE_REPLACE, [self.width, 0, 0, 0])
+	if align == "right":
+	    top.property_change("_NET_WM_STRUT", "CARDINAL", 32, 
+	                        gtk.gdk.PROP_MODE_REPLACE, [0, self.width, 0, 0])
 
 
     def apply_config(self, configTree):
 	self.configTree = configTree
 	#Dock geometry
-	height = int(configTree.findtext("window/height"))
-        width = int(configTree.findtext("window/width"))
-	self.set_size_request(width, height)
+	self.height = int(configTree.findtext("window/height"))
+        self.width = int(configTree.findtext("window/width"))
+	self.set_size_request(self.width, self.height)
 	#Apply background color
         color = gtk.gdk.color_parse(configTree.findtext("window/bg"))
 	self.modify_bg(gtk.STATE_NORMAL, color)
@@ -34,9 +44,12 @@ class OmniDOCKGUI(gtk.Window):
 	opacity = float(configTree.findtext("window/opacity"))
 	self.set_opacity(opacity)
 	#Run alignment method to place dock on screen
-        self.align_to(configTree.findtext("window/align"))
+        align = configTree.findtext("window/align")
+        self.align_to(align)
         #And finally add the table (where the magic happens)
         self.add_docktable()
+        self.show_all()
+        self.apply_strut(align)
          
 
     def align_to(self, alignment):
@@ -49,6 +62,8 @@ class OmniDOCKGUI(gtk.Window):
             xpos = screenW - int(self.configTree.findtext("window/width"))
             ypos = 0
         elif alignment == "left":
+	    #self.get_root_window().property_change("_NET_WM_STRUT", "CARDINAL", 32, gtk.gdk.PROP_MODE_REPLACE, [50, 0, 0, 0])
+	    
 	    xpos = 0
 	    ypos = 0
 	else:
